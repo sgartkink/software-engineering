42aeca871262a185bf49c4535bab46d268935946
damczi17 <72543223+damczi17@users.noreply.github.com>
2020-11-04 17:09:14 +0100

    Update graph_drawing.h

--- graph_drawing.h
+++ graph_drawing.h
@@ -3,15 +3,31 @@
 
 
 void making_files_graph(std::string graph_name, std::list<std::string> files_list, our_map map) {
-	std::ofstream grapg_name("graph.gv");
-    grapg_name << "digraph files\n{\n";
+	std::ofstream grapg_name("files_graph.gv");
+    grapg_name << "digraph files_graph\n{\n";
     for (auto it = files_list.begin(); it != files_list.end(); ++it) {
             for (auto const& include : map[*it].includes) {
-                grapg_name << *it <<" " << map[*it].size << "b" << "->" << include << "\n";
+                grapg_name << '"' << *it << "_" << map[*it].size << "b" << '"' << "->" << '"' << include << '"' << "\n";
             }
     }
     grapg_name << "}";
     grapg_name.close();
-
-
+    //for Windows
+    std::ofstream script("files_graph.bat");
+    script << "@ECHO OFF\n";
+    script << "dot -Tpng files_graph.gv -o files_graph.png";
+    script.close();
+    system("files_graph.bat");
+    system("files_graph.png");
+    
+    
+    //for Linux
+    /*
+    std::ofstream script("files_graph.sh");
+    script << "#/bin/bash\n";
+    script << "dot -Tpng files_graph.gv -o files_graph.png";
+    script.close();
+    system("files_graph.sh");
+    system("files_graph.png");
+    */
 }
