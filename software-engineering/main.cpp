664d79eccb2d1906ec85f8cc41c09370983c503f
damczi17 <72543223+damczi17@users.noreply.github.com>
2020-11-10 20:18:34 +0100

    Update main.cpp

--- software-engineering/main.cpp
+++ software-engineering/main.cpp
@@ -3,6 +3,7 @@
 #include "functions/display_connections.h"
 #include "functions/display_includes.h"
 #include "structs/ProjectConnections.h"
+#include "graph_drawing.h"
 
 int main()
 {
@@ -11,11 +12,15 @@ int main()
 
     search::our_map map = search::create_map(files_list);
 
-    display_includes(files_list, map);
+    making_files_graph("files_graph",files_list, map);
 
     ProjectConnections projectConnections(files_list);
-    
-    display_connections(projectConnections);
+
+    making_functions_graph("functions_graph", projectConnections);
+
+    making_namespaces_graph("namespaces_graph", projectConnections);
+
+    //display_connections(projectConnections);
     
     return 0;
 }
