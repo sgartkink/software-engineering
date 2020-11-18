3cb051635b7f23bea540d8f6ea8718fc10b92a54
damczi17 <72543223+damczi17@users.noreply.github.com>
2020-11-18 15:30:17 +0100

    Add files via upload

--- paradigm_graph.h
+++ paradigm_graph.h
@@ -3,18 +3,9 @@
 #include <map>
 #include <vector>
 
-struct paradigmID {
-	std::string filename;
-	int fileID;
-};
-std::vector<paradigmID> filesID;
+std::map<std::string, int> filesID;
 std::map<int, std::vector<int>> IDmap;
-int findID(std::string x) {
-	for (int i = 0; i < filesID.size(); i++) {
-		if (filesID[i].filename == x)
-			return filesID[i].fileID;
-	}
-}
+
 void files_graph(std::string file_name, std::list<std::string> files_list, search::our_map map) {
 	std::stringstream name, name2;
 	std::string s, s2, name3, name4;
@@ -23,27 +14,23 @@ void files_graph(std::string file_name, std::list<std::string> files_list, searc
 		name << *it;
 		s = name.str();
 		name3 = getname(s);
-		filesID.push_back({name3,usecaseid});
+		filesID[name3] = usecaseid;
 		usecaseid++;
 	}
+	usecaseid = 1;
 	for (auto it = files_list.begin(); it != files_list.end(); ++it) {
-		name << *it;
-		s = name.str();
-		name3 = getname(s);
-		filesID.push_back({ name3,usecaseid });
 		for (auto const& include : map[*it].includes) {
-			int j = 0;
 			name2 << include;
 			s2 = name2.str();
 			name4 = getname(s2);
-			IDmap[usecaseid].push_back(findID(name4));
-			j++;
+			IDmap[usecaseid].push_back(filesID.find(name4)->second);
 		}
 		usecaseid++;
 	}
+
 	for (auto const& x : IDmap) {
 		std::cout << x.first << ":" << std::endl;
-		for(int i = 0; i < x.second.size();i++)
+		for (int i = 0; i < x.second.size(); i++)
 			std::cout << x.second[i] << std::endl;
 	}
 }
