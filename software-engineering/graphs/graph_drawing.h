2dd77229fc7647803d38b5b2aa78e129d5c2c686
damczi17 <72543223+damczi17@users.noreply.github.com>
2021-01-20 21:40:33 +0100

    Update graph_drawing.h

--- software-engineering/graphs/graph_drawing.h
+++ software-engineering/graphs/graph_drawing.h
@@ -1,10 +1,9 @@
 
 #include "../structs/FileStruct.h"
+#include "../functions/git_utils.h"
 #include "./graph_connections.h"
 #include "./graph_includes.h"
 #include "./graph_bash.h"
-
-
 
 
 void general_graph(int number, std::list<std::string> files_list, std::map<std::string, FileStruct> map, ProjectConnections projectConnections) {
@@ -13,24 +12,23 @@ void general_graph(int number, std::list<std::string> files_list, std::map<std::
     std::string x = graph_name + ".gv";
     std::ofstream file(x);
     bool created = 1;
-    
     if (number == 1) {
         file << "digraph files_graph\n{\nsubgraph cluster0\n{\nnode[style = filled,color = white];\nstyle = filled;\n\color = lightgrey;\n";
         file.close();
         files_graph(x, files_list, map);
         file.open(x, std::ios::out | std::ios::app);
-        file << "label = " << '"' << "Files graph" << '"' << ";\n}\n}";
+        file << "label = " << '"' << "Files graph" << '"' << ";\n}";
+        file << "label = " << '"' << "Git commit ID: " << get_last_commit_id("", "C://software-engineering/software-engineering") <<'"' << ";\n}";
         file.close();
     }
     else if (number == 2) {
         file << "digraph files_graph\n{\nsubgraph cluster0\n{\nnode[style = filled,color = white];\nstyle = filled;\n\color = lightgrey;\n";
         file.close();
-        //std::vector<std::string> functions_list = our_funtions_list(projectConnections.getFunctionConnections());
         funtions_files_to_graph(projectConnections.getFunctionConnections(), x);
         functions_graph(x, projectConnections);
-        //functions_graph(x, projectConnections, functions_list);
         file.open(x, std::ios::out | std::ios::app);
-        file << "label = " << '"' << "Functions graph" << '"' << ";\n}\n}";
+        file << "label = " << '"' << "Functions graph" << '"' << ";\n}";
+        file << "label = " << '"' << "Git commit ID: " << get_last_commit_id("", "C://software-engineering/software-engineering") << '"' << ";\n}";
         file.close();
     }
     else if (number == 3) {
@@ -38,19 +36,18 @@ void general_graph(int number, std::list<std::string> files_list, std::map<std::
         file.close();
         namespaces_graph(x, projectConnections);
         file.open(x, std::ios::out | std::ios::app);
-        file << "label = " << '"' << "Namespaces graph" << '"' << ";\n}\n}";
+        file << "label = " << '"' << "Namespaces graph" << '"' << ";\n}";
+        file << "label = " << '"' << "Git commit ID: " << get_last_commit_id("", "C://software-engineering/software-engineering") << '"' << ";\n}";
         file.close();
         created = 1;
     }
     else if (number == 12 || number == 21) {
         file << "digraph " << graph_name << "\n{\n";
         file.close();
-        //std::vector<std::string> functions_list = funtions_files_to_graph(projectConnections.getFunctionConnections(), x);
-        //functions_graph(x, projectConnections, functions_list);
         funtions_files_to_graph(projectConnections.getFunctionConnections(), x);
         functions_graph(x, projectConnections);
         file.open(x, std::ios::out | std::ios::app);
-        file << "}";
+        file << "label = " << '"' << "Git commit ID: " << get_last_commit_id("", "C://software-engineering/software-engineering") << '"' << ";\n}";
         file.close();
     }
     else if (number == 13 || number == 31) {
@@ -60,7 +57,7 @@ void general_graph(int number, std::list<std::string> files_list, std::map<std::
         files_graph(x, files_list, map);
         namespaces_graph(x, projectConnections);
         file.open(x, std::ios::out | std::ios::app);
-        file << "label = " << '"' << graph_name << '"' << ";\n}";
+        file << "label = " << '"' << "Git commit ID: " << get_last_commit_id("", "C://software-engineering/software-engineering") << '"' << ";\n}";
         file.close();
     }
     else if (number == 23 || number == 32) {
@@ -71,7 +68,7 @@ void general_graph(int number, std::list<std::string> files_list, std::map<std::
         }
         functions_graph(x, projectConnections);
         file.open(x, std::ios::out | std::ios::app);
-        file << "}";
+        file << "label = " << '"' << "Git commit ID: " << get_last_commit_id("", "C://software-engineering/software-engineering") << '"' << ";\n}";
         file.close();
     }
     else if (number == 123 || number == 132 || number == 213 || number == 231 || number == 312 || number == 321) {
@@ -84,7 +81,7 @@ void general_graph(int number, std::list<std::string> files_list, std::map<std::
         }
         functions_graph(x, projectConnections);
         file.open(x, std::ios::out | std::ios::app);
-        file << "}";
+        file << "label = " << '"' << "Git commit ID: " << get_last_commit_id("", "C://software-engineering/software-engineering") << '"' << ";\n}";
         file.close();
     }
     else if (number == 4) {
@@ -92,7 +89,7 @@ void general_graph(int number, std::list<std::string> files_list, std::map<std::
         file.close();
         funtions_files_to_graph(projectConnections.getFunctionConnections(), x, "reversed");
         file.open(x, std::ios::out | std::ios::app);
-        file << "}";
+        file << "label = " << '"' << "Git commit ID: " << get_last_commit_id("", "C://software-engineering/software-engineering") << '"' << ";\n}";
         file.close();
     }
     else {
